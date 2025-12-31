50ce08e04b66f842e7697fb937a7719ba9d80c94
Eminenz07 <eminenceadeoluwa@gmail.com>
2025-12-31 01:01:13 +0100

    Fix: Added robust unique slug generation to Event model to prevent 500 collisions

--- events/models.py
+++ events/models.py
@@ -33,7 +33,13 @@ class Event(models.Model):
     
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Event.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
