0978329d47b4cf6fd22653ccc7825ea4ade75eab
Eminenz07 <eminenceadeoluwa@gmail.com>
2026-01-12 10:08:34 +0100

    feat: Enforce unique email addresses during signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,3 +11,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('A user with that email already exists.')
+        return email
